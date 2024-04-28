b9a2b85845773ed9ebb107bc322f4c2167c141df
DavMInass <narek.shaghoyan@tumo.org>
2024-04-28 17:25:10 +0400

    hosting

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require("express");
-const path = require('path')
-const http = require('http')
+const path = require("path");
+const http = require("http");
+const bodyParser = require("body-parser");
+const routes = require("./app/routes/routes");
+const initializeSocket = require("./app/socket/socketHandler");
+require("dotenv").config();
+
 
 const app = express();
-const PORT = 3000
+const server = http.createServer(app);
 
-app.use(express.static(path.join(__dirname,
-"public")))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", routes);
 
-const server = http.createServer(app)
+initializeSocket(server);
 
-server.listen(PORT, () => {
-console.log(`server is running on port ${PORT}`);
+server.listen(3000, () => {
+  console.log("Server listens to port 3000");
 });
